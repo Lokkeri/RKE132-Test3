30a9fe6215e1ade0dc84128c09805612f82782dd
Kristjan. <kristjan.seli@tktk.ee>
2024-04-24 06:05:06 +0300

    add test 3 code

---
+++ test 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Sisesta oma hinne(A,B,C,E või F):");
+string hinne = Console.ReadLine().ToUpper();
+
+if (hinne == "A")
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (hinne == "B")
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (hinne == "C")
+{
+    Console.WriteLine("Hea!");
+}
+else if (hinne == "D")
+{
+    Console.WriteLine("Rahuldav!");
+}
+else if (hinne == "E")
+{
+    Console.WriteLine("Kasin!");
+}
+else if (hinne == "F")
+{
+    Console.WriteLine("Puudulik!");
+}
+else
+{
+    Console.WriteLine("Vale väärtus!");
+}
